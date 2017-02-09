b4278dd4059ede2b940459ba4e6a8d13f15de7f3
Francis Fulgencio <ffulgencio@gmail.com>
2017-02-08 22:36:46 -0400

    para subir archivos al repo

---
+++ app/src/main/java/com/example/francis/calculadoraandroid/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.francis.calculadoraandroid;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public TextView txtResultado;
+    Button btn1,btn2,btn3,btn4,btn5,btn6, btn7, btn8, btn9, btn0, btnLimpiar;
+    Button btnSumar, btnRestar, btnMultiplicar, btnDividir, btnIgual;
+    public boolean punto=false;
+    public String Texto ="";
+    public double n1,n2;
+    public Boolean operacionElegida = false;
+    public String operacion = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtResultado = (TextView) findViewById(R.id.textView);
+        btn1 = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+        btn4 = (Button) findViewById(R.id.button4);
+        btn5 = (Button) findViewById(R.id.button5);
+        btn6 = (Button) findViewById(R.id.button6);
+        btn7 = (Button) findViewById(R.id.button7);
+        btn8 = (Button) findViewById(R.id.button8);
+        btn9 = (Button) findViewById(R.id.button9);
+        btn0 = (Button) findViewById(R.id.button0);
+        btnLimpiar = (Button) findViewById(R.id.btnLimpiar);
+        btnSumar = (Button) findViewById(R.id.btnSumar);
+        btnIgual =(Button) findViewById(R.id.btnIgual);
+
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnLimpiar.setOnClickListener(this);
+        btnSumar.setOnClickListener(this);
+        btnIgual.setOnClickListener(this);
+        //btn1.setOnClickListener(this);
+
+
+
+    }
+
+    public void Operar(String s){
+        n1 = Double.valueOf(Texto);
+        Texto ="";
+        operacionElegida = true;
+        operacion = s;
+        txtResultado.setText("0");
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Button b = (Button) v;
+
+        if (txtResultado.getText().equals("0")) {
+            txtResultado.setText(b.getText());
+            //Texto += (String) txtResultado.getText();
+
+        }else {
+            if (v.getId() != R.id.btnDividir && v.getId() != R.id.btnSumar){
+                Texto += (String) b.getText();
+                txtResultado.setText(Texto);
+                if (b.getId() == R.id.btnLimpiar) {
+                    txtResultado.setText("0");
+
+                }else
+                    Texto += (String) txtResultado.getText();
+            }
+        }
+
+
+
+        if (b.getId() == R.id.btnSumar){
+            if (operacionElegida ==false) {
+                this.Operar("suma");
+            }
+
+        }else if (b.getId() == R.id.btnRestar){
+            if (operacionElegida ==false) {
+
+                this.Operar("resta");
+
+            }
+
+        }if (b.getId() == R.id.btnMultiplicar){
+            if (operacionElegida ==false) {
+
+                this.Operar("multiplicar");
+
+            }
+
+        }else if (b.getId() == R.id.btnDividir) {
+                if (operacionElegida == false) {
+
+                    this.Operar("dividir");
+
+                }
+        }else{
+            if (operacionElegida == true) {
+                if (b.getId() == R.id.btnIgual) {
+                        n2 = Double.valueOf(Texto);
+                        txtResultado.setText(String.valueOf(n1+n2));
+                        Texto="";
+                        operacionElegida = false;
+
+
+                }
+
+            }
+
+        }
+
+    }
+}
